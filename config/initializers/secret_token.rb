3637028b9ca16736b0d887918e57054d64f2d3a9
Coulton Bunney <cabunney@gmail.com>
2014-11-08 13:27:36 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hueandwry::Application.config.secret_key_base = '36054232634d570dac522bcc7710fbb4b94f1c76998dd37c137039f572dd40a7cdba199f8350d5a79029257b1dd20407a513fcb10190a71eba435c9bc79917ee'
